eb6e785aeeb76677ab922f75fa1f0b1fd3e6e004
rees-thalmic <rees.simmons@thalmic.com>
2018-03-20 11:57:39 -0400

    fixing package

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(name='morphsnakes',
       version='1.0',
       description='Git Repo for morphsnakes algorithm',
       author='P. Márquez Neila <p.mneila@upm.es>',
-      package_dir={'':'src'},
-      packages=find_packages('src'),
+      packages=['morphsnakes'],
       install_requires=['numpy', 'scipy']
 )
